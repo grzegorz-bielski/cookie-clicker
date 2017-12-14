fca5361ccb26b19680077faa01c05691bdc20e49
Grzegorz Bielski <pesiok@gmail.com>
2017-12-14 20:46:05 +0100

    sw path fix

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,7 @@
 import { defineConstants } from 'webpack-blocks';
 
 export default function (config, env, helpers) {
-	const PUBLIC_PATH = env.production ? '/cookie-clicker' : '';
+	const PUBLIC_PATH = env.production ? '/cookie-clicker/' : '/';
 	config.output.publicPath = PUBLIC_PATH;
 	config.plugins.push(defineConstants({ PUBLIC_PATH }));
 }
