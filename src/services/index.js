aa68fce03e06037097034c6d46c52aa84ad46717
Grzegorz Bielski <pesiok@gmail.com>
2017-12-20 21:42:16 +0100

    prerender build

--- src/services/index.js
+++ src/services/index.js
@@ -6,24 +6,31 @@ import { getInitialState, transformDbState } from './game/util';
 import GameService from './game/game';
 import DbService from './database/db';
 
-// inject dependencies and initialize the app
-
 export let app;
-export const observable = new ObservableService();
+export let observable;
 
-const dbService = new DbService({
-	driver: idb,
-	name: 'cookie-clicker-db',
-	stores: [{ store: 'state', key: 'name' }]
-});
+// run only in browser
+if (typeof window !== 'undefined') {
+	// inject dependencies and initialize the app
+	
+	observable = new ObservableService();
 
-dbService.readAll('state').then(state => {
-	app = new GameService({
-		observable,
-		dbService,
-		saveInterval: 2,
-		state: (!state || state.length <= 0)
-			? getInitialState()
-			: transformDbState(state[0])
+	const dbService = new DbService({
+		driver: idb,
+		name: 'cookie-clicker-db',
+		stores: [{ store: 'state', key: 'name' }]
 	});
-});
+
+	dbService.readAll('state').then(state => {
+		app = new GameService({
+			observable,
+			dbService,
+			saveInterval: 2,
+			state: (!state || state.length <= 0)
+				? getInitialState()
+				: transformDbState(state[0])
+		});
+	});
+}
+
+
